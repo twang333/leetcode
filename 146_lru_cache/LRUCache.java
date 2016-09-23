76ffa9263c2f10f9d540d6455cf66be72d066f17
Tom Wang <tuo.wang@oracle.com>
2016-09-23 19:59:45 +0800

    fix LRU bug

--- 146_lru_cache/LRUCache.java
+++ 146_lru_cache/LRUCache.java
@@ -2,22 +2,22 @@ public class LRUCache {
   private class Node {
     int key, value;
     Node prev, next;
-    
+
     public Node() {
       key = 0;
       value = 0;
     }
-    
+
     public Node(int k, int v) {
-      key = 0;
+      key = k;
       value = v;
     }
   }
-  
+
   private int capacity, count;
   private Map<Integer, Node> map;
   private Node head, tail;
-  
+
   public LRUCache(int capacity) {
     this.count = 0;
     this.capacity = capacity;
@@ -27,7 +27,7 @@ public class LRUCache {
     head.next = tail;
     tail.prev = head;
   }
-  
+
   public int get(int key) {
     Node node = map.get(key);
     if(null == node) {
@@ -37,31 +37,37 @@ public class LRUCache {
     addAtHead(node);
     return node.value;
   }
-  
+
   public void set(int key, int value) {
-    if(count >= capacity) {
-      Node node = tail.prev;
-      node.prev.next = node.next;
-      node.next.prev = node.prev;
-      node.prev = null;
-      node.next = null;
-      map.remove(node.key);
+    Node node = map.get(key);
+    if(null == node) {
+      if(count >= capacity) {
+        Node nodeToRemove = tail.prev;
+        nodeToRemove.prev.next = nodeToRemove.next;
+        nodeToRemove.next.prev = nodeToRemove.prev;
+        nodeToRemove.prev = null;
+        nodeToRemove.next = null;
+        map.remove(nodeToRemove.key);
+      } else {
+        count ++;
+      }
+      node = new Node(key, value);
+      addAtHead(node);
+      map.put(key, node);
     } else {
-      count ++;
+      node.value = value;
+      remove(node);
+      addAtHead(node);
     }
-    Node node = new Node(key, value);
-    addAtHead(node);
-    map.put(key, node);
   }
-  
-  
+
   private void remove(Node node) {
     node.prev.next = node.next;
     node.next.prev = node.prev;
     node.prev = null;
     node.next = null;
   }
-  
+
   private void addAtHead(Node node) {
     Node after = head.next;
     node.next = after;
