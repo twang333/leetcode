d262f2c91a113284f2e0e1c6e98ebbc8768213ff
Tom Wang <tuo.wang@oracle.com>
2016-10-19 19:27:11 +0800

    add unit test

--- 57_insert_interval/insert_interval.rb
+++ 57_insert_interval/insert_interval.rb
@@ -49,3 +49,21 @@ def is_overlapped?(interval_1, interval_2)
     true
   end
 end
+
+# class Interval
+#   attr_accessor :start, :end
+#   def initialize(s=0, e=0)
+#     @start = s
+#     @end = e
+#   end
+
+#   def ==(interval)
+#     self.start == interval.start && self.end == interval.end
+#   end
+# end
+
+# p [Interval.new(1,2)] == insert([], Interval.new(1,2))
+# p [Interval.new(-2,-1),Interval.new(1,3)] == insert([Interval.new(1,3)],Interval.new(-2,-1))
+# p [Interval.new(1,3),Interval.new(5,6)] == insert([Interval.new(1,3)],Interval.new(5,6))
+# p [Interval.new(1,5),Interval.new(6,9)] == insert([Interval.new(1,3),Interval.new(6,9)],Interval.new(2,5))
+# p [Interval.new(1,2),Interval.new(3,10),Interval.new(12,16)] == insert([Interval.new(1,2),Interval.new(3,5),Interval.new(6,7),Interval.new(8,10),Interval.new(12,16)],Interval.new(4,9))
