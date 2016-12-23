00a0e9f94ba4bdcab5ab4e1fa97d132e71140fd7
twang333 <wangtuo2005@gmail.com>
2016-12-23 19:43:22 +0800

    add k smallest paris using heap

--- 373_k_smallest_pairs/k_smallest_pairs.rb
+++ 373_k_smallest_pairs/k_smallest_pairs.rb
@@ -3,60 +3,133 @@
 # @param {Integer} k
 # @return {Integer[][]}
 def k_smallest_pairs(nums1, nums2, k)
-  nums1_size = nums1.size
-  nums2_size = nums2.size
-  if nums1_size == 0 || nums2_size == 0 || k == 0 
-    return []
+  return [] if nums1.size == 0 || nums2.size == 0 || k == 0
+  minHeap = MinHeap.new do
+    nums2.map do |item|
+      Node.new nums1[0], item
+    end
   end
 
-  possible_k = [k, nums1_size * nums2_size].min
   result = []
-
-  i = 0
-  j = 0
-  result.push [nums1[i], nums2[j]]
-
-  if nums1.last > nums2.last
-    i_flag = true
-  else
-    j_flag = true
-  end
-
-  while(result.size < possible_k)
-    if i_flag && (i+1) % nums1_size == 0
-      result.push [ nums1[i%nums1_size], nums2[(j+1)%nums2_size] ]
-      j += 1
-      next
-    end
-
-    if j_flag && (j+1) % nums2_size == 0
-      result.push [ nums1[(i+1)%nums1_size], nums2[j%nums2_size] ]
-      i += 1
-      next
-    end
-
-    if (i+1) % nums1_size == 0 || (j+1) % nums2_size == 0
-      result.push [ nums1[(i+1)%nums1_size], nums2[(j+1)%nums2_size] ]
-      i += 1
-      j += 1
-      next
-    end
-
-    candidate1 = [ nums1[(i+1)%nums1_size], nums2[j%nums2_size] ]
-    candidate2 = [ nums1[i%nums1_size], nums2[(j+1)%nums2_size] ] 
-
-    if candidate1.reduce(:+) > candidate2.reduce(:+)
-      result.push candidate2
-      j += 1
+  [k, nums2.size * nums1.size].min.times do |i|
+    root = minHeap.root
+    if root.index < nums1.size - 1
+      new_node = Node.new nums1[root.index + 1], root.value_2, root.index + 1
+      minHeap.replace new_node
     else
-      result.push candidate1
-      i += 1
+      minHeap.extract
     end
-
+    result << [root.value_1, root.value_2]
   end
 
   return result
-    
+end
+
+class Node
+  include Comparable
+  attr_accessor :value_1,  :value_2, :index
+
+  def initialize(value_1, value_2, index = 0)
+    @value_1 = value_1
+    @value_2 = value_2
+    @index   = index
+  end
+
+  def <=>(other)
+    (value_1 + value_2) <=> (other.value_1 + other.value_2)
+  end
+
+end
+
+class MinHeap
+  attr_accessor :array
+  def initialize(array = [])
+    @array = array
+    if block_given?
+      @array = yield
+    end
+    heapify
+  end
+
+  def heapify
+    return if array.size < 2
+    array[1..-1].each_with_index do |item, index|
+      heapify_up(index)
+    end
+  end
+
+  def heapify_up(index = nil)
+    index ||= array.size - 1
+
+    parent_index = (index - 1)/2
+    smallest = index
+
+    if parent_index > 0 && array[parent_index] > array[smallest]
+      smallest = parent_index
+    end
+
+    if smallest != index
+      swap(index, smallest)
+      heapify_up(smallest)
+    end
+  end
+
+  def heapify_down(index = nil)
+    index ||= 0
+    size = array.size
+
+    left_child = 2 * index + 1
+    right_child = 2 * index + 2
+    smallest = index
+
+    if left_child < size && array[smallest] > array[left_child]
+      smallest = left_child
+    end
+
+    if right_child < size && array[smallest] > array[right_child]
+      smallest = right_child
+    end
+
+    if index != smallest
+      swap(index, smallest)
+      heapify_down(smallest)
+    end
+
+  end
+
+  def insert(item)
+    array << item
+    heapify_up
+  end
+
+  def extract
+    return nil if array.size < 1
+    first = array.first
+    array[0] = array.last
+    array.pop
+    heapify_down
+    return first
+  end
+
+  def replace(item)
+    return nil if array.size < 1
+    first = array.first
+    array[0] = item
+    heapify_down
+    return first
+  end
+
+  def root
+    array.first
+  end
+
+  private
+  def swap(i, j)
+    temp = array[j]
+    array[j] = array[i]
+    array[i] = temp
+  end
+
 end
 
 nums1 = [1,7,11]
@@ -86,3 +159,11 @@ k = 10
 p "Given nums1 = [1,1,2], nums2 = [1,2,3],  k = 10"
 p "Expected: [1,1],[1,1],[2,1],[1,2],[1,2],[2,2],[1,3],[1,3],[2,3]"
 p "Actual: #{k_smallest_pairs(nums1, nums2, k)}"
+
+nums1 = [1,7,11]
+nums2 = [2,4,6]
+k = 3
+p "Given nums1 = #{nums1}, nums2 = #{nums2},  k = #{k}"
+p "Expected: [[1,2],[1,4],[1,6]]"
+p "Actual: #{k_smallest_pairs(nums1, nums2, k)}"
+
