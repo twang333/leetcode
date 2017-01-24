974f69e2c5e9606ac728553eecf60923571540ec
twang333 <wangtuo2005@gmail.com>
2017-01-24 17:11:56 +0800

    add 5 test

---
+++ other_problems/5_closure_test.js
@@ -0,0 +1,20 @@
+var fibonacci = function (i) {
+  console.log("fibonacci")
+  memo = [0, 1, 2];
+
+  var fib = function(i) {
+    var result = memo[i];
+    if(typeof result !== "number") {
+      console.log("fib")
+      result = fib(i-1) + fib(i-2);
+      memo[i] = result;
+    }
+    return result;
+  };
+
+  return fib;
+}
+
+for(var i = 0; i < 10; i++) {
+  console.log(fibonacci()(i))
+}
