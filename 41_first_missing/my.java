ea1b85a12b76f6a347cf5c8cb462969fc89ca9af
twang333 <wangtuo2005@gmail.com>
2017-02-18 11:32:50 +0800

    add 41 my

---
+++ 41_first_missing/my.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int firstMissingPositive(int[] nums) {
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] <= 0) nums[i] = nums.length+1;
+        }
+        
+        for(int i = 0; i < nums.length; i++) {
+            int refer = Math.abs(nums[i]) - 1;
+            if(refer < nums.length && nums[refer] > 0) nums[refer] = -nums[refer];
+        }
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] > 0) return i+1;
+        }
+        return nums.length+1;
+    }
+}
