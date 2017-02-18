0e75f1cf75ad7cb16469369d3a109498d9a57d78
twang333 <wangtuo2005@gmail.com>
2017-02-18 12:22:02 +0800

    add 448 other

---
+++ 448_disappeared_array/other.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++) {
+            int refer = Math.abs(nums[i]) - 1;
+            if(nums[refer] > 0) nums[refer] = -nums[refer];
+        }
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] > 0) res.add(i+1);
+        }
+        
+        return res;
+    }
+}
