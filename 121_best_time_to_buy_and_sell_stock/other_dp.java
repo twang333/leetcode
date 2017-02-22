3e995a91e304e9da1b3f6531adc26b3f26f1cc38
twang333 <wangtuo2005@gmail.com>
2017-02-22 22:52:32 +0800

    add 121 other dp

---
+++ 121_best_time_to_buy_and_sell_stock/other_dp.java
@@ -0,0 +1,12 @@
+// 0(n)
+// https://discuss.leetcode.com/topic/19853/kadane-s-algorithm-since-no-one-has-mentioned-about-this-so-far-in-case-if-interviewer-twists-the-input
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int maxCur = 0, maxSoFar = 0;
+        for(int i = 1; i < prices.length; i++) {
+            maxCur = Math.max(0, maxCur += prices[i] - prices[i-1]);
+            maxSoFar = Math.max(maxCur, maxSoFar);
+        }
+        return maxSoFar;
+    }
+}
