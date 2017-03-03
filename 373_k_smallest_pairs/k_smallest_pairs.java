7306320f87d467f5298301294b6175a649d04624
twang333 <wangtuo2005@gmail.com>
2017-03-04 00:29:43 +0800

    update 373.java

--- 373_k_smallest_pairs/k_smallest_pairs.java
+++ 373_k_smallest_pairs/k_smallest_pairs.java
@@ -1,11 +1,8 @@
 public class Solution {
   public List<int[]> kSmallestPairs(int[] nums1, int[] nums2, int k) {
     List<int[]> res = new ArrayList<>();
-    PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>(){
-      public int compare(int[] a, int[] b) {
-        return a[0] + a[1] - b[0] - b[1];
-      }
-    });
+    if(nums1.length == 0 || nums2.length == 0) return res;
+    PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[0]+a[1]-b[0]-b[1]);
 
     for (int i = 0;  i < nums2.length; i++ ) {
       pq.offer(new int[]{nums1[0], nums2[i], 0});
@@ -14,7 +11,8 @@ public class Solution {
     while( k-- > 0 && !pq.isEmpty()) {
       int[] pair = pq.poll();
       res.add(new int[]{pair[0], pair[1]});
-      if(pair[2] <= nums1.length - 2) pq.offer(new int[]{nums1[pair[2]+1], pair[1], pair[2]+1});
+      if(pair[2] == nums1.length - 1) continue;
+      pq.offer(new int[]{nums1[pair[2]+1], pair[1], pair[2]+1});
     }
 
     return res;
