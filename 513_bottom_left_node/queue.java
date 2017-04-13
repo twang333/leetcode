33d0b50db878a444fbc07fca027b64391eda8c2b
twang333 <wangtuo2005@gmail.com>
2017-04-13 18:32:29 +0800

    add solution for 513

--- 513_bottom_left_node/queue.java
+++ 513_bottom_left_node/queue.java
@@ -18,3 +18,30 @@ public class Solution {
         return stacks.pop().get(0);
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int res = root.val;
+        while(!queue.isEmpty()) {
+            res = queue.peek().val;
+            int size = queue.size();
+            for(int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if(node.left != null) queue.offer(node.left);
+                if(node.right != null) queue.offer(node.right);
+            }
+        }
+        return res;
+    }
+}
